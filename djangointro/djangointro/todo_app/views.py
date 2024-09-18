ecc01a06e969339fe137ad9bc0fabfee067c1a25
Anton Simeonov <aplamenov@gmail.com>
2024-09-18 19:09:59 +0300

    new view

--- djangointro/djangointro/todo_app/views.py
+++ djangointro/djangointro/todo_app/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def my_view(request):
+    return HttpResponse('<h1>Hello!</h1>')
--- djangointro/djangointro/urls.py
+++ djangointro/djangointro/urls.py
@@ -2,7 +2,10 @@
 STEP 1: Create a project
 STEP 2: Create an app
 STEP 3: Add the app to INSTALlED_APPS
-
+STEP 4: Replace DB settings with Postgres DB settings
+STEP 5: Enter credentials
+STEP 6: Install psycopg2
+STEP 7: Create data base
 """
 from django.contrib import admin
 from django.urls import path
