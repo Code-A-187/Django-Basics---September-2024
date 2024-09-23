432324195edc963485ec90c3c15ac9c7cf07d5e4
Anton Simeonov <aplamenov@gmail.com>
2024-09-23 16:34:25 +0300

    uuid, pk, slug path

--- urlsAndViews/urlsAndViews/departments/urls.py
+++ urlsAndViews/urlsAndViews/departments/urls.py
@@ -6,8 +6,9 @@ from urlsAndViews.departments.views import index
 urlpatterns = [
     path('', views.index),
     path('<int:pk>/', views.view_with_int_pk),
-    path('<variable>/', views.view_with_name),
-
-
+    path('<int:pk>/<slug:slug>/', views.view_with_slug),
+    path('<variable>/', views.view_with_name),  # matches till /
+    path('<path:variable>', views.view_with_name),  # matches after as well
+    # path('<uuid:id>/', some,view)
     # path('<param>/', views.view_with_args_kwargs),
 ]
--- urlsAndViews/urlsAndViews/departments/views.py
+++ urlsAndViews/urlsAndViews/departments/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from urlsAndViews.departments.models import Department
+
 
 def index(request):
     return HttpResponse('<h1>Hello, world!</h1>')
@@ -18,3 +20,9 @@ def view_with_args_kwargs(request, *args, **kwargs):
 
 def view_with_int_pk(request, pk):
     return HttpResponse(f'<h1>Int pk with pk: {pk}</h1>')
+
+
+def view_with_slug(request, pk, slug):
+    department = Department.objects.get(pk=pk, slug=slug)
+
+    return HttpResponse(f"<h1>Department from slug: {department}</h1>")
