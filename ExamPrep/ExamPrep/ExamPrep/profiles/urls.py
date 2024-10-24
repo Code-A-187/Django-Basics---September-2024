d71c26a70a86a93f078fd6a6fdaed32f746b6a3e
Anton Simeonov <aplamenov@gmail.com>
2024-10-24 20:19:59 +0300

    feat: profile details and delete

---
+++ ExamPrep/ExamPrep/ExamPrep/profiles/urls.py
@@ -0,0 +1,7 @@
+from ExamPrep.profiles import views
+from django.urls import path
+
+urlpatterns = [
+    path('details/', views.ProfileDetailView.as_view(), name='profile-details'),
+    path('delete/', views.ProfileDeleteView.as_view(), name='profile-delete')
+]
--- ExamPrep/ExamPrep/ExamPrep/urls.py
+++ ExamPrep/ExamPrep/ExamPrep/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ExamPrep.common.urls')),
-    path('album/', include('ExamPrep.albums.urls'))
+    path('album/', include('ExamPrep.albums.urls')),
+    path('profile/', include('ExamPrep.profiles.urls'))
 ]
