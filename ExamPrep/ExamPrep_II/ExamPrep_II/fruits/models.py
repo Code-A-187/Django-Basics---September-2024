92f79a7d5730d3999095bf42407e839ebd008eda
Anton Simeonov <aplamenov@gmail.com>
2024-10-25 14:01:01 +0300

    feat: fruits model and validators

--- ExamPrep/ExamPrep_II/ExamPrep_II/fruits/models.py
+++ ExamPrep/ExamPrep_II/ExamPrep_II/fruits/models.py
@@ -1,4 +1,33 @@
+from ExamPrep_II.fruits.validators import OnlyLettersValidator
+from django.core.validators import MinLengthValidator
 from django.db import models
 
+
 class Fruit(models.Model):
-    pass
+    name = models.CharField(
+        unique=True,
+        max_length=30,
+        validators=[
+            MinLengthValidator(2),
+            OnlyLettersValidator(),
+        ],
+        error_messages={
+            'unique': "This fruit name is already in use! Try a new one."
+        }
+    )
+
+    image_url = models.URLField()
+
+    description = models.TextField()
+
+    nutrition = models.TextField(
+        blank=True,
+        null=True,
+    )
+
+    owner = models.ForeignKey(
+        to="profiles.Profile",
+        on_delete=models.CASCADE,
+        related_name='fruits',
+    )
+
