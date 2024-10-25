31a53a93391856a1c812ace6bbc2460500b58587
Anton Simeonov <aplamenov@gmail.com>
2024-10-25 19:44:22 +0300

    feat: fruit details

--- ExamPrep/ExamPrep_II/ExamPrep_II/fruits/urls.py
+++ ExamPrep/ExamPrep_II/ExamPrep_II/fruits/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from ExamPrep_II.fruits import views
 
 urlpatterns = [
-    path('create/', views.FruitCreateView.as_view(), name='fruit-create')
+    path('create/', views.FruitCreateView.as_view(), name='fruit-create'),
+    path('<int:fruitId>/', include([
+        path('details/', views.FruitDetailsView.as_view(), name='fruit-details'),
+        path('edit/', views.FruitEditView.as_view(), name='fruit-edit')
+    ]))
 ]
--- ExamPrep/ExamPrep_II/ExamPrep_II/fruits/views.py
+++ ExamPrep/ExamPrep_II/ExamPrep_II/fruits/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from ExamPrep_II.fruits.forms import FruitCreateForm
 from ExamPrep_II.fruits.models import Fruit
@@ -16,3 +16,16 @@ class FruitCreateView(CreateView):
     def form_valid(self, form):
         form.instance.owner = get_user_obj()
         return super().form_valid(form)
+
+
+class FruitDetailsView(DetailView):
+    model = Fruit
+    pk_url_kwarg = 'fruitId'
+    template_name = 'fruits/details-fruit.html'
+
+
+class FruitEditView(UpdateView):
+    model: Fruit
+    pk_url_kwarg = 'fruitId'
+    template_name = 'fruits/edit-fruit.html'
+
